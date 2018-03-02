f2d1ddf799bd905cc6b37a0afbd7a64d681a6fd6
Lucas <lucas.mech@hotmail.com>
2018-03-02 06:02:54 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+
+        <div class="login">
+            <div class="avatar-user">
+                <img src="img/avatar-user.png" alt="">
+            </div>
+            <div class="form">
+
+                <h1>Login</h1>
+                <form action="">
+
+                    <input type="email"  class="email form-control" placeholder="Entre com seu email" name="email"><br>
+                    <label class="lb-email" for="email">E-mail*</label>
+                    <input type="password" class="password form-control" name="password" placeholder="Entre com sua senha"><br>
+                    <label class="lb-password" for="password">Senha*</label>
+                    <button class="button btn btn-success btn-lg">Logar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
