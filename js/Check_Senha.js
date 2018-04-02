d193371cf071c0c62aaf59abe8b2c7ea7521ec74
Lucas <lucas.mech@hotmail.com>
2018-04-02 18:29:42 -0300

    Add validador de telefone

--- js/Check_Senha.js
+++ js/Check_Senha.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
 
+
+
 $('form').submit(function () {
     var senha = $('input[name=password]').val();
     var comSenha = $('input[name=confirm-password]').val();
 
     if (senha != comSenha){
+        $('#email-error').remove('#phone-error');
 
-
-        $('#error-pass').html('<p>Senhas Diferentes</p>').css("text-align","center");
+        $('#error-pass').html('<p>Senhas Diferentes</p>');
         $('#error-pass').css("color","red");
         $('#error-pass').css("color","red");
-        $('#error-pass').css("font-size","23px");
-        $('#error-pass').css("letter-spacing","3px");
+
+
         $('input[name=password]').css("border","2px solid red");
         $('input[name=confirm-password]').css("border","2px solid red");
         $('button').css("top","0");
